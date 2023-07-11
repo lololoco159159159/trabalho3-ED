4367f218d322f0bd2625e3b0b1698d07dad3966c
lololoco159159159 <lorenzorfjogos123@gmail.com>
2023-07-11 18:34:43 -0300

    Level Order Traversal Iterativo

--- traversal/main.c
+++ traversal/main.c
@@ -37,8 +37,8 @@ int main(){
 
     //Vector *v = binary_tree_inorder_traversal(bt);
     //Vector *v = binary_tree_preorder_traversal(bt);
-    Vector *v = binary_tree_postorder_traversal(bt);
-    //Vector *v = binary_tree_levelorder_traversal(bt);
+    //Vector *v = binary_tree_postorder_traversal(bt);
+    Vector *v = binary_tree_levelorder_traversal(bt);
 
     //Vector *v = binary_tree_inorder_traversal_recursive(bt);
     //Vector *v = binary_tree_preorder_traversal_recur//sive(bt);
